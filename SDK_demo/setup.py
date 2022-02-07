297770c30e8f0cdd18822cde642d3b890f3f43f4
Daretowin <59953727+Daretowin@users.noreply.github.com>
2022-02-07 15:13:30 +0800

    Add files via upload

---
+++ SDK_demo/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+	name = "hello_world",
+	version = "1.0",
+	url = 'https://github.com/snowroll/python-sdk.git',#
+	long_description = open('README.md').read(),
+	packages = find_packages(),
+)
